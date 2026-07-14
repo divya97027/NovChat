e681ef8c97b728ed7ece155c511ee883984a1b45
Divya <divya97027@gmail.com>
2026-07-14 11:04:10 +0530

    Initial commit: Spring Boot project setup with MySQL connection

---
+++ src/main/java/com/novchat/novchat_backend/NovchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.novchat.novchat_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovchatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovchatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novchat/novchat_backend/NovchatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novchat.novchat_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovchatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
